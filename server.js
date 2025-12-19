72e305633a1c3e3a719f542b3d5635da330acd77
Trustra-ai <alysiam488@gmail.com>
2025-12-19 12:06:59 +0100

    Update MongoDB connection with dbName option

--- server.js
+++ server.js
@@ -1,17 +1,5 @@
-const express = require('express');
-const mongoose = require('mongoose'); // must be first
-const cors = require('cors');
-const dotenv = require('dotenv');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-
-// Load environment variables
-dotenv.config();
-
-// Mongoose 7+ global setting
-mongoose.set('strictQuery', true);
-
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGO_URI, {
+  dbName: 'TrustraFx',
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
